c90eaf360918c09b22ef7dc46bec8ca0696e9e8a
Yanbei Chen <yanbeic@gmail.com>
2018-09-18 13:13:29 +0100

    update

--- scripts/download_prepare_datasets.sh
+++ scripts/download_prepare_datasets.sh
@@ -5,6 +5,16 @@ mkdir rawdata/svhn
 mkdir rawdata/cifar10
 mkdir rawdata/cifar100
 
+mkdir results
+mkdir results/svhn
+mkdir results/cifar10
+mkdir results/cifar100
+
+mkdir tfrecords
+mkdir tfrecords/svhn
+mkdir tfrecords/cifar10
+mkdir tfrecords/cifar100
+
 cd rawdata/svhn
 wget http://ufldl.stanford.edu/housenumbers/train_32x32.mat
 wget http://ufldl.stanford.edu/housenumbers/test_32x32.mat
